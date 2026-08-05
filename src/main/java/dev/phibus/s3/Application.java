42db14be176a119e8caeccf07fd0fca01fde0f63
phibus-dev <4hsmcs7prj@privaterelay.appleid.com>
2026-08-05 11:31:19 +0300

    Add Spring Boot application entry point

---
+++ src/main/java/dev/phibus/s3/Application.java
@@ -0,0 +1,27 @@
+package dev.phibus.s3;
+
+import java.util.concurrent.Executor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@SpringBootApplication
+@EnableAsync
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(name = "testExecutor")
+    Executor testExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);
+        executor.setQueueCapacity(20);
+        executor.setThreadNamePrefix("s3-test-");
+        executor.initialize();
+        return executor;
+    }
+}
