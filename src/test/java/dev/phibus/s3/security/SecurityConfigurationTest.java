b0b8f3853f28ee0ef6a39d95822761f17bbde50c
phibus-dev <4hsmcs7prj@privaterelay.appleid.com>
2026-08-20 18:17:58 +0300

    Test Keycloak client role mapping

--- src/test/java/dev/phibus/s3/security/SecurityConfigurationTest.java
+++ src/test/java/dev/phibus/s3/security/SecurityConfigurationTest.java
@@ -10,14 +10,34 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 class SecurityConfigurationTest {
     @Test
-    void mapsSupportedRealmRolesAndIgnoresUnknownRoles() {
+    void mapsSupportedClientRolesAndIgnoresRealmAndUnknownRoles() {
         Jwt jwt = new Jwt("token", Instant.now(), Instant.now().plusSeconds(60),
-                Map.of("alg", "none"), Map.of("sub", "user-1",
-                "realm_access", Map.of("roles", List.of("admin", "viewer", "unrelated"))));
+                Map.of("alg", "none"), Map.of(
+                "sub", "user-1",
+                "realm_access", Map.of("roles", List.of("ADMIN")),
+                "resource_access", Map.of(
+                        "s3-perf", Map.of("roles", List.of("admin", "viewer", "unrelated")),
+                        "other-client", Map.of("roles", List.of("operator")))));
 
-        var authorities = new SecurityConfiguration.KeycloakRoleConverter().convert(jwt);
+        var converter = new SecurityConfiguration.KeycloakRoleConverter(
+                "s3-perf", "ADMIN", "OPERATOR", "VIEWER");
+        var authorities = converter.convert(jwt);
 
         assertThat(authorities).extracting(Object::toString)
                 .containsExactlyInAnyOrder("ROLE_ADMIN", "ROLE_VIEWER");
+    }
+
+    @Test
+    void supportsCustomClientRoleNames() {
+        Jwt jwt = new Jwt("token", Instant.now(), Instant.now().plusSeconds(60),
+                Map.of("alg", "none"), Map.of("sub", "user-1",
+                "resource_access", Map.of("s3-perf", Map.of("roles", List.of("evo-admin", "evo-operator")))));
+
+        var converter = new SecurityConfiguration.KeycloakRoleConverter(
+                "s3-perf", "EVO-ADMIN", "EVO-OPERATOR", "EVO-VIEWER");
+        var authorities = converter.convert(jwt);
+
+        assertThat(authorities).extracting(Object::toString)
+                .containsExactlyInAnyOrder("ROLE_ADMIN", "ROLE_OPERATOR");
     }
 }
